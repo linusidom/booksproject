bc4ca5cd21e1c877e129327568687949b0264244
linusidom <linusidom@gmail.com>
2018-07-30 11:27:17 +0700

    updated requirements again again

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
@@ -32,7 +33,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-ALLOWED_HOSTS = ['thawing-brook-81399']
+ALLOWED_HOSTS = ['modisidom']
 
 
 # Application definition
