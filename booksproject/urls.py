efd16bbcf84878f6538e5fbf9a7a2c27318aa127
linusidom <linusidom@gmail.com>
2018-08-09 00:16:03 +0700

    updated responsive CSS and password reset functionality

--- booksproject/urls.py
+++ booksproject/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
     url(r'^api_exercises/', include('exercises.api.urls',namespace = 'api_exercises')),
 
     # Password Reset
-    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html', email_template_name = 'accounts/password_reset_email.html', subject_template_name='accounts/password_reset_subject.txt', domain='modisidom.herokuapp.com'), name='password_reset'),
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html', email_template_name = 'accounts/password_reset_email.html', subject_template_name='accounts/password_reset_subject.txt'), name='password_reset'),
     url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
